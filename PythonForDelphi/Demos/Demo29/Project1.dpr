b487245b38ccb0b16bc0cedd5fb77fb32b846eae
pyscripter <pyscripter@e41fde0b-5a56-0410-8151-d5964863ceb2>
2012-08-28 01:09:14 +0000

    Fixed partially for Unicode

--- PythonForDelphi/Demos/Demo29/Project1.dpr
+++ PythonForDelphi/Demos/Demo29/Project1.dpr
@@ -1,3 +1,6 @@
+// JCL_DEBUG_EXPERT_GENERATEJDBG OFF
+// JCL_DEBUG_EXPERT_INSERTJDBG OFF
+// JCL_DEBUG_EXPERT_DELETEMAPFILE OFF
 program Project1;
 
 uses
@@ -5,7 +8,7 @@ uses
   Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
-
+{$R XP_UAC.RES}
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
