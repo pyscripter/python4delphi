357834105b7a8aaec854a848a3bfd3f4b7072d4e
Christian Ulrich <info@cu-tec.de>
2016-09-14 22:46:42 +0200

    Fix compiling in old Lazarus (0.9.3x)

--- PythonForDelphi/Components/P4DFpc.pas
+++ PythonForDelphi/Components/P4DFpc.pas
@@ -2,21 +2,14 @@
   This source is only used to compile and install the package.
  }
 
-unit P4DFpc;
+unit P4DFpc; 
 
 interface
 
 uses
-  PythonEngine, MethodCallBack, TinyWideStrings, VarPyth, WrapDelphiClasses, 
-  WrapDelphiTypes, LazarusPackageIntf;
+    PythonEngine, MethodCallBack, TinyWideStrings, VarPyth, WrapDelphiClasses, 
+  WrapDelphiTypes;
 
 implementation
 
-procedure Register;
-begin
-  RegisterUnit('PythonEngine', @PythonEngine.Register);
-end;
-
-initialization
-  RegisterPackage('P4DFpc', @Register);
 end.
