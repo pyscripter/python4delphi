f4510474d7a12594e07924e2f6ddf23a9ed5cf51
pyscripter <pyscripter@gmail.com>
2018-02-03 13:07:55 +0200

    Set8087CW not available in ARM processors

--- PythonForDelphi/Components/Sources/Core/PythonEngine.pas
+++ PythonForDelphi/Components/Sources/Core/PythonEngine.pas
@@ -9720,6 +9720,7 @@ end;
 procedure MaskFPUExceptions(ExceptionsMasked : boolean;
   MatchPythonPrecision : Boolean);
 begin
+ {$IFNDEF CPUARM}
   if MatchPythonPrecision then begin
     if ExceptionsMasked then
       Set8087CW($1232 or $3F)
@@ -9731,6 +9732,7 @@ begin
     else
       Set8087CW($1332);
   end;
+ {$ENDIF}
 end;
 
 {$IFDEF MSWINDOWS}
