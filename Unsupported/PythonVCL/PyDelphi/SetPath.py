0d482bebceb5b0d00a0f59098a47895d631b139c
pyscripter <pyscripter@gmail.com>
2020-04-24 05:51:33 +0300

    Remove old and unsupported stuff.  There is no reason to confuse users.

--- Unsupported/PythonVCL/PyDelphi/SetPath.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# (Un)comment the line corresponding to your delphi version 
-# and adjust the path to your current installation
-
-sys.path.append("d:\users\Morgan\PythonEngine\Lib\Delphi\D3")
-
-#sys.path.append("d:\users\Morgan\PythonEngine\Lib\Delphi\D4")
