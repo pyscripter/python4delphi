4632fb3b8c89e4db559d147eed53a828114f602a
Lucas Moura Belo <lucas.belo@live.com>
2020-11-18 08:21:43 -0300

    Separate packages into run-time and design-time

---
+++ Source/PythonRegistration.pas
@@ -0,0 +1,19 @@
+unit PythonRegistration;
+
+interface
+
+procedure Register();
+
+implementation
+
+uses
+  Classes, PythonEngine, WrapDelphi;
+
+procedure Register();
+begin
+  RegisterComponents('Python', [TPythonEngine, TPythonType, TPythonModule,
+                                TPythonDelphiVar, TPythonInputOutput,
+                                TPyDelphiWrapper]);
+end;
+
+end.
