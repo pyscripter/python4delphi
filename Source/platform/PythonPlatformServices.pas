e64be6cf59c71a2b022c619b11226b49eb33b885
Lucas Moura Belo <lucas.belo@live.com>
2020-11-19 17:06:53 -0300

    Adding framework/platform independent dll loader service

--- Source/platform/PythonPlatformServices.pas
+++ Source/platform/PythonPlatformServices.pas
@@ -6,6 +6,17 @@ uses
   System.Generics.Collections, SysUtils, PythonException;
 
 type
+  IPythonLoaderService = interface
+    ['{C223FB58-E5FC-407F-A061-8E6DABEF1DB7}']
+    function IsHandleValid(const AHandle: THandle): boolean;
+    function LoadDll(const ADllPath: string; out AHandle: THandle): boolean;
+    procedure UnloadDll(var AHandle: THandle);
+    function Import(const AHandle: THandle; const AFuncName: AnsiString; ACanFail: boolean = true): pointer;
+
+    procedure FatalMsgDlg(const AMsg: string);
+    procedure FatalAbort(const AMsg: string);
+  end;
+
   TPythonPlatformServices = class
   private
     class var FInstance: TPythonPlatformServices;
