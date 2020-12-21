3720e6ef8564b13f6e08b16a798e890b232fec62
Lucas Moura Belo <lucas.belo@live.com>
2020-12-21 15:53:22 -0300

    Adding TScaledLayout wrapper

--- Source/fmx/WrapFmxLayouts.pas
+++ Source/fmx/WrapFmxLayouts.pas
@@ -16,6 +16,16 @@ type
     property DelphiObject: TLayout read GetDelphiObject write SetDelphiObject;
   end;
 
+  TPyDelphiScaledLayout = class(TPyDelphiControl)
+  private
+    function  GetDelphiObject: TScaledLayout;
+    procedure SetDelphiObject(const Value: TScaledLayout);
+  public
+    class function  DelphiObjectClass : TClass; override;
+    // Properties
+    property DelphiObject: TScaledLayout read GetDelphiObject write SetDelphiObject;
+  end;
+
 implementation
 
 { Register the wrappers, the globals and the constants }
@@ -26,23 +36,6 @@ type
     procedure RegisterWrappers(APyDelphiWrapper : TPyDelphiWrapper); override;
     procedure DefineVars(APyDelphiWrapper : TPyDelphiWrapper); override;
   end;
-
-{ TPyDelphiLayout }
-
-class function TPyDelphiLayout.DelphiObjectClass: TClass;
-begin
-  Result := TLayout;
-end;
-
-function TPyDelphiLayout.GetDelphiObject: TLayout;
-begin
-  Result := TLayout(inherited DelphiObject);
-end;
-
-procedure TPyDelphiLayout.SetDelphiObject(const Value: TLayout);
-begin
-  inherited DelphiObject := Value;
-end;
 
 { TLayoutsRegistration }
 
@@ -61,6 +54,41 @@ procedure TLayoutsRegistration.RegisterWrappers(
 begin
   inherited;
   APyDelphiWrapper.RegisterDelphiWrapper(TPyDelphiLayout);
+  APyDelphiWrapper.RegisterDelphiWrapper(TPyDelphiScaledLayout);
+end;
+
+{ TPyDelphiLayout }
+
+class function TPyDelphiLayout.DelphiObjectClass: TClass;
+begin
+  Result := TLayout;
+end;
+
+function TPyDelphiLayout.GetDelphiObject: TLayout;
+begin
+  Result := TLayout(inherited DelphiObject);
+end;
+
+procedure TPyDelphiLayout.SetDelphiObject(const Value: TLayout);
+begin
+  inherited DelphiObject := Value;
+end;
+
+{ TPyDelphiScaledLayout }
+
+class function TPyDelphiScaledLayout.DelphiObjectClass: TClass;
+begin
+  Result := TScaledLayout;
+end;
+
+function TPyDelphiScaledLayout.GetDelphiObject: TScaledLayout;
+begin
+  Result := TScaledLayout(inherited DelphiObject);
+end;
+
+procedure TPyDelphiScaledLayout.SetDelphiObject(const Value: TScaledLayout);
+begin
+  inherited DelphiObject := Value;
 end;
 
 initialization
