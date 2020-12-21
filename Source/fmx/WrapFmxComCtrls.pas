c5130baeb49cf5aa3f6be7bf09a50eafb4d7b82e
Lucas Moura Belo <lucas.belo@live.com>
2020-12-21 10:55:02 -0300

    Adding TMultiView wrapper

--- Source/fmx/WrapFmxComCtrls.pas
+++ Source/fmx/WrapFmxComCtrls.pas
@@ -26,6 +26,16 @@ type
     property DelphiObject: TCustomMultiView read GetDelphiObject write SetDelphiObject;
   end;
 
+  TPyDelphiMultiView = class(TPyDelphiCustomMultiView)
+  private
+    function GetDelphiObject: TMultiView;
+    procedure SetDelphiObject(const Value: TMultiView);
+  public
+    class function DelphiObjectClass: TClass; override;
+    // Properties
+    property DelphiObject: TMultiView read GetDelphiObject write SetDelphiObject;
+  end;
+
 implementation
 
 uses
@@ -58,6 +68,7 @@ begin
   inherited;
   APyDelphiWrapper.RegisterDelphiWrapper(TPyDelphiTabControl);
   APyDelphiWrapper.RegisterDelphiWrapper(TPyDelphiCustomMultiView);
+  APyDelphiWrapper.RegisterDelphiWrapper(TPyDelphiMultiView);
 end;
 
 { TPyDelphiTabControl }
@@ -91,6 +102,23 @@ end;
 
 procedure TPyDelphiCustomMultiView.SetDelphiObject(
   const Value: TCustomMultiView);
+begin
+  inherited DelphiObject := Value;
+end;
+
+{ TPyDelphiMultiView }
+
+class function TPyDelphiMultiView.DelphiObjectClass: TClass;
+begin
+  Result := TMultiView;
+end;
+
+function TPyDelphiMultiView.GetDelphiObject: TMultiView;
+begin
+  Result := TMultiView(inherited DelphiObject);
+end;
+
+procedure TPyDelphiMultiView.SetDelphiObject(const Value: TMultiView);
 begin
   inherited DelphiObject := Value;
 end;
