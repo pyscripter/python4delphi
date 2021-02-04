55ce2e1379bdb5e05268a01ea589c809990a62e1
Idan Miara <idan@miara.com>
2021-02-04 18:14:13 +0200

    PythonEngine.pas - Move TDynamicDLL to DynamicDll.pas * DynamicDll.pas - make GetDllPath virtual to allow override in TPythonInterface * PythonEngine.pas - GetDllPath override with a special behavior for the PythonDLL (IsPythonVersionRegistered)

--- Source/PythonEngine.pas
+++ Source/PythonEngine.pas
@@ -90,7 +90,8 @@ uses
   SysUtils,
   SyncObjs,
   Variants,
-  MethodCallBack;
+  MethodCallBack,
+  DynamicDll;
 
 {$IF not Defined(FPC) and (CompilerVersion >= 23)}
 const
@@ -906,11 +907,6 @@ type
 
   // Components' exceptions
   EDLLLoadError  = class(Exception);
-  EDLLImportError = class(Exception)
-    public
-      WrongFunc : AnsiString;
-      ErrorCode : Integer;
-  end;
 
   // Python's exceptions
   EPythonError   = class(Exception)
@@ -1120,70 +1116,6 @@ type
     property RawOutput: Boolean read FRawOutput write FRawOutput;
   end;
 
-//-------------------------------------------------------
-//--                                                   --
-//--      Base class:  TDynamicDll                     --
-//--                                                   --
-//-------------------------------------------------------
-
-type
-  TDynamicDll = class(TComponent)
-  private
-    function IsAPIVersionStored: Boolean;
-    function IsDllNameStored: Boolean;
-    function IsRegVersionStored: Boolean;
-    procedure SetDllName(const Value: string);
-  protected
-    FDllName            : string;
-    FDllPath            : string;
-    FAPIVersion         : Integer;
-    FRegVersion         : string;
-    FAutoLoad           : Boolean;
-    FAutoUnload         : Boolean;
-    FFatalMsgDlg        : Boolean;
-    FFatalAbort         : Boolean;
-    FDLLHandle          : THandle;
-    FUseLastKnownVersion: Boolean;
-    FOnBeforeLoad       : TNotifyEvent;
-    FOnAfterLoad        : TNotifyEvent;
-    FOnBeforeUnload     : TNotifyEvent;
-
-    function  Import(const funcname: AnsiString; canFail : Boolean = True): Pointer;
-    procedure Loaded; override;
-    procedure BeforeLoad; virtual;
-    procedure AfterLoad; virtual;
-    procedure BeforeUnload; virtual;
-    function  GetQuitMessage : string; virtual;
-    procedure DoOpenDll(const aDllName : string); virtual;
-    function  GetDllPath : string;
-
-  public
-    // Constructors & Destructors
-    constructor Create(AOwner: TComponent); override;
-    destructor  Destroy;                    override;
-
-    // Public methods
-    procedure OpenDll(const aDllName : string);
-    function  IsHandleValid : Boolean;
-    procedure LoadDll;
-    procedure UnloadDll;
-    procedure Quit;
-
-    // Public properties
-  published
-    property AutoLoad : Boolean read FAutoLoad write FAutoLoad default True;
-    property AutoUnload : Boolean read FAutoUnload write FAutoUnload default True;
-    property DllName : string read FDllName write SetDllName stored IsDllNameStored;
-    property DllPath : string read FDllPath write FDllPath;
-    property APIVersion : Integer read FAPIVersion write FAPIVersion stored IsAPIVersionStored;
-    property RegVersion : string read FRegVersion write FRegVersion stored IsRegVersionStored;
-    property FatalAbort :  Boolean read FFatalAbort write FFatalAbort default True;
-    property FatalMsgDlg : Boolean read FFatalMsgDlg write FFatalMsgDlg default True;
-    property UseLastKnownVersion: Boolean read FUseLastKnownVersion write FUseLastKnownVersion default True;
-    property OnAfterLoad : TNotifyEvent read FOnAfterLoad write FOnAfterLoad;
-    property OnBeforeLoad : TNotifyEvent read FOnBeforeLoad write FOnBeforeLoad;
-    property OnBeforeUnload : TNotifyEvent read FOnBeforeUnload write FOnBeforeUnload;
-  end;
 
 //-------------------------------------------------------
 //--                                                   --
@@ -1218,6 +1150,7 @@ type
     function  GetQuitMessage : string; override;
     procedure CheckPython;
     function  GetUnicodeTypeSuffix : string;
+    function  GetDllPath : string; override;
 
   public
     // define Python flags. See file pyDebug.h
@@ -2900,210 +2833,6 @@ begin
   FLinesPerThread.Strings[ GetCurrentThreadSlotIdx ] := string(FLine_Buffer);
 end;
 
-(*******************************************************)
-(**                                                   **)
-(**            class TDynamicDll                      **)
-(**                                                   **)
-(*******************************************************)
-
-procedure TDynamicDll.DoOpenDll(const aDllName : string);
-begin
-  if not IsHandleValid then
-  begin
-    FDllName := aDllName;
-    {$IFDEF MSWINDOWS}
-    FDLLHandle := SafeLoadLibrary(
-      {$IFDEF FPC}
-      PAnsiChar(AnsiString(GetDllPath+DllName))
-      {$ELSE}
-      GetDllPath+DllName
-      {$ENDIF});
-    {$ELSE}
-    //Linux: need here RTLD_GLOBAL, so Python can do "import ctypes"
-    FDLLHandle := THandle(dlopen(PAnsiChar(AnsiString(GetDllPath+DllName)),
-      RTLD_LAZY+RTLD_GLOBAL));
-    {$ENDIF}
-  end;
-end;
-
-function  TDynamicDll.GetDllPath : string;
-{$IFDEF MSWINDOWS}
-var
-  AllUserInstall: Boolean;
-{$ENDIF}
-begin
-  Result := DllPath;
-
-  {$IFDEF MSWINDOWS}
-  if DLLPath = '' then begin
-    IsPythonVersionRegistered(RegVersion, Result, AllUserInstall);
-  end;
-  {$ENDIF}
-
-  if Result <> '' then
-  begin
-    Result := IncludeTrailingPathDelimiter(Result);
-  end;
-end;
-
-procedure  TDynamicDll.OpenDll(const aDllName : string);
-var
-  s : string;
-begin
-  UnloadDll;
-
-  BeforeLoad;
-
-  FDLLHandle := 0;
-
-  DoOpenDll(aDllName);
-
-  if not IsHandleValid then begin
-    {$IFDEF MSWINDOWS}
-    s := Format('Error %d: Could not open Dll "%s"',[GetLastError, DllName]);
-    {$ELSE}
-    s := Format('Error: Could not open Dll "%s"',[DllName]);
-    {$ENDIF}
-    if FatalMsgDlg then
-      {$IFDEF MSWINDOWS}
-      MessageBox( GetActiveWindow, PChar(s), 'Error', MB_TASKMODAL or MB_ICONSTOP );
-      {$ELSE}
-      WriteLn(ErrOutput, s);
-      {$ENDIF}
-
-    if FatalAbort then
-      Quit;
-  end else
-    AfterLoad;
-end;
-
-constructor TDynamicDll.Create(AOwner: TComponent);
-begin
-  inherited;
-  FFatalMsgDlg          := True;
-  FFatalAbort           := True;
-  FAutoLoad             := True;
-  FUseLastKnownVersion  := True;
-end;
-
-destructor TDynamicDll.Destroy;
-begin
-  if AutoUnload then
-    UnloadDll;
-  inherited;
-end;
-
-function TDynamicDll.Import(const funcname: AnsiString; canFail : Boolean = True): Pointer;
-var
-  E : EDllImportError;
-  {$IF not Defined(FPC) and not Defined(MSWINDOWS)}
-  S : string;
-  {$IFEND}
-begin
-  {$IF Defined(FPC) or Defined(MSWINDOWS)}
-  Result := GetProcAddress( FDLLHandle, PAnsiChar(funcname) );
-  {$ELSE}
-  S := string(funcname);
-  Result := GetProcAddress( FDLLHandle, PWideChar(S) );
-  {$IFEND}
-  if (Result = nil) and canFail then begin
-    {$IFDEF MSWINDOWS}
-    E := EDllImportError.CreateFmt('Error %d: could not map symbol "%s"', [GetLastError, funcname]);
-    E.ErrorCode := GetLastError;
-    {$ELSE}
-    E := EDllImportError.CreateFmt('Error: could not map symbol "%s"', [funcname]);
-    {$ENDIF}
-    E.WrongFunc := funcname;
-    raise E;
-  end;
-end;
-
-procedure TDynamicDll.Loaded;
-begin
-  inherited;
-  if AutoLoad and not (csDesigning in ComponentState) then
-    LoadDll;
-end;
-
-function  TDynamicDll.IsHandleValid : Boolean;
-begin
-{$IFDEF MSWINDOWS}
-  Result := (FDLLHandle >= 32);
-{$ELSE}
-  Result := FDLLHandle <> 0;
-{$ENDIF}
-end;
-
-procedure TDynamicDll.LoadDll;
-begin
-  OpenDll( DllName );
-end;
-
-procedure TDynamicDll.UnloadDll;
-begin
-  if IsHandleValid then begin
-    BeforeUnload;
-    FreeLibrary(FDLLHandle);
-    FDLLHandle := 0;
-  end;
-end;
-
-procedure TDynamicDll.BeforeLoad;
-begin
-  if Assigned( FOnBeforeLoad ) then
-    FOnBeforeLoad( Self );
-end;
-
-procedure TDynamicDll.AfterLoad;
-begin
-  if Assigned( FOnAfterLoad ) then
-    FOnAfterLoad( Self );
-end;
-
-procedure TDynamicDll.BeforeUnload;
-begin
-  if Assigned( FOnBeforeUnload ) then
-    FOnBeforeUnload( Self );
-end;
-
-function  TDynamicDll.GetQuitMessage : string;
-begin
-  Result := Format( 'Dll %s could not be loaded. We must quit.', [DllName]);
-end;
-
-procedure TDynamicDll.Quit;
-begin
-  if not( csDesigning in ComponentState ) then begin
-{$IFDEF MSWINDOWS}
-    MessageBox( GetActiveWindow, PChar(GetQuitMessage), 'Error', MB_TASKMODAL or MB_ICONSTOP );
-    ExitProcess( 1 );
-{$ELSE}
-    WriteLn(ErrOutput, GetQuitMessage);
-    Halt( 1 );
-{$ENDIF}
-  end;
-end;
-
-function TDynamicDll.IsAPIVersionStored: Boolean;
-begin
-  Result := not UseLastKnownVersion;
-end;
-
-function TDynamicDll.IsDllNameStored: Boolean;
-begin
-  Result := not UseLastKnownVersion;
-end;
-
-function TDynamicDll.IsRegVersionStored: Boolean;
-begin
-  Result := not UseLastKnownVersion;
-end;
-
-procedure TDynamicDll.SetDllName(const Value: string);
-begin
-  FDllName := Value;
-end;
-
 
 (*******************************************************)
 (**                                                   **)
@@ -3122,6 +2851,26 @@ begin
   FAPIVersion := PYTHON_KNOWN_VERSIONS[i].APIVersion;
   FRegVersion := PYTHON_KNOWN_VERSIONS[i].RegVersion;
   FAutoUnload := True;
+end;
+
+function TPythonInterface.GetDllPath: string;
+{$IFDEF MSWINDOWS}
+var
+  AllUserInstall: Boolean;
+{$ENDIF}
+begin
+  Result := FDllPath;
+
+  {$IFDEF MSWINDOWS}
+  if Result = '' then begin
+    IsPythonVersionRegistered(RegVersion, Result, AllUserInstall);
+  end;
+  {$ENDIF}
+
+  if Result <> '' then
+  begin
+    Result := IncludeTrailingPathDelimiter(Result);
+  end;
 end;
 
 procedure TPythonInterface.AfterLoad;
