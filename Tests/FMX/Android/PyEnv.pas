ec4d215c91000013849f5208057732e427d1b8f6
Lucas Belo <lucas.belo@live.com>
2021-06-03 14:15:44 -0700

    Test case - Check up Python environment on Android

---
+++ Tests/FMX/Android/PyEnv.pas
@@ -0,0 +1,73 @@
+unit PyEnv;
+
+interface
+
+uses
+  DUnitX.TestFramework, PythonEngine;
+
+type
+  {$M+}
+  [TestFixture]
+  TPyEnvTest = class
+  private
+    FPythonEngine: TPythonEngine;
+  public
+    [SetupFixture]
+    procedure SetupFixture;
+    [TearDownFixture]
+    procedure TearDownFixture;
+    [Test]
+    procedure TestLibFile();
+    [Test]
+    procedure TestZipFile();
+    [Test]
+    procedure TestExtraction();
+    [Test]
+    procedure TestConfigure();
+  end;
+
+implementation
+
+uses
+  PythonLoad;
+
+{ TPyEnvTest }
+
+procedure TPyEnvTest.SetupFixture;
+begin
+  FPythonEngine := TPythonEngine.Create(nil);
+  FPythonEngine.Name := 'PythonEngine';
+end;
+
+procedure TPyEnvTest.TearDownFixture;
+begin
+  FPythonEngine.Free();
+end;
+
+procedure TPyEnvTest.TestConfigure;
+begin
+  TPythonLoad.Configure(FPythonEngine);
+  FPythonEngine.LoadDll;
+  Assert.IsTrue(FPythonEngine.IsHandleValid());
+end;
+
+procedure TPyEnvTest.TestExtraction;
+begin
+  TPythonLoad.Extract();
+  Assert.IsTrue(TPythonLoad.HasPythonDist());
+end;
+
+procedure TPyEnvTest.TestLibFile;
+begin
+  Assert.IsTrue(TPythonLoad.HasPythonLib());
+end;
+
+procedure TPyEnvTest.TestZipFile;
+begin
+  Assert.IsTrue(TPythonLoad.HasPythonZip());
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TPyEnvTest);
+
+end.
