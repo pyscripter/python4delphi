b38cceb2eb7c09a1bb1eb0957fd2a51ee233d689
Lucas Belo <lucas.belo@live.com>
2021-09-09 14:35:48 -0700

    CameraPreviewDemo updates

--- Demos/FMX/Android/CameraPreviewDemo/MainForm.pas
+++ Demos/FMX/Android/CameraPreviewDemo/MainForm.pas
@@ -1,51 +1,51 @@
-(**************************************************************************)
-(*                                                                        *)
-(* Module:  Unit 'MainForm'     Copyright (c) 2021                        *)
-(*                                                                        *)
-(*                                  Lucas Moura Belo - lmbelo             *)
-(*                                  lucas.belo@live.com                   *)
-(*                                  Brazil                                *)
-(*                                                                        *)
-(*                                  PyScripter                            *)
-(*                                  e-mail: pyscripter@gmail.com          *)
-(*                                                                        *)
-(*  Project pages:      https://github.com/Embarcadero/python4delphi      *)
-(*                      https://github.com/pyscripter/python4delphi       *)
-(**************************************************************************)
-(*  Functionality:  It creates a form at run-time and adds a              *)
-(*                  TCameraComponent on it. Also creates a                *)
-(*                  Start/Stop camera for a previewer.                    *)
-(*                                                                        *)
-(**************************************************************************)
-(* This source code is distributed with no WARRANTY, for no reason or use.*)
-(* Everyone is allowed to use and change this code free for his own tasks *)
-(* and projects, as long as this header and its copyright text is intact. *)
-(* For changed versions of this code, which are public distributed the    *)
-(* following additional conditions have to be fullfilled:                 *)
-(* 1) The header has to contain a comment on the change and the author of *)
-(*    it.                                                                 *)
-(* 2) A copy of the changed source has to be sent to the above E-Mail     *)
-(*    address or my then valid address, if this is possible to the        *)
-(*    author.                                                             *)
-(* The second condition has the target to maintain an up to date central  *)
-(* version of the component. If this condition is not acceptable for      *)
-(* confidential or legal reasons, everyone is free to derive a component  *)
-(* or to generate a diff file to my or other original sources.            *)
-(**************************************************************************)
-
+(* ************************************************************************ *)
+(*                                                                          *)
+(* Module:  Unit 'MainForm'     Copyright (c) 2021                          *)
+(*                                                                          *)
+(* Lucas Moura Belo - lmbelo                                                *)
+(* lucas.belo@live.com                                                      *)
+(* Brazil                                                                   *)
+(*                                                                          *)
+(* PyScripter                                                               *)
+(* e-mail: pyscripter@gmail.com                                             *)
+(*                                                                          *)
+(* Project pages:      https://github.com/Embarcadero/python4delphi         *)
+(* https://github.com/pyscripter/python4delphi                              *)
+(* ************************************************************************ *)
+(* Functionality:  It creates a form at run-time and adds a                 *)
+(* TCameraComponent on it. Also creates a                                   *)
+(* Start/Stop camera for a previewer.                                       *)
+(*                                                                          *)
+(* ************************************************************************ *)
+(* This source code is distributed with no WARRANTY, for no reason or use.  *)
+(* Everyone is allowed to use and change this code free for his own tasks   *)
+(* and projects, as long as this header and its copyright text is intact.   *)
+(* For changed versions of this code, which are public distributed the      *)
+(* following additional conditions have to be fullfilled:                   *)
+(* 1) The header has to contain a comment on the change and the author of   *)
+(* it.                                                                      *)
+(* 2) A copy of the changed source has to be sent to the above E-Mail       *)
+(* address or my then valid address, if this is possible to the             *)
+(* author.                                                                  *)
+(* The second condition has the target to maintain an up to date central    *)
+(* version of the component. If this condition is not acceptable for        *)
+(* confidential or legal reasons, everyone is free to derive a component    *)
+(* or to generate a diff file to my or other original sources.              *)
+(* ************************************************************************ *)
 {$I Definition.inc}
-
 unit MainForm;
 
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants, System.Messaging,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
   FMX.Ani, FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo,
-  PythonEngine, FMX.PythonGUIInputOutput, System.Actions, FMX.ActnList,
-  FMX.Objects, FMX.Layouts, FMX.Platform, AppEnvironment, ProgressFrame,
-  System.Threading, WrapDelphi, WrapDelphiFmx;
+  PythonEngine, FMX.PythonGUIInputOutput, System.Actions, System.Permissions,
+  FMX.ActnList, FMX.Objects, FMX.Layouts, FMX.Platform, FMX.Media,
+  AppEnvironment,
+  ProgressFrame, System.Threading, WrapDelphi, WrapDelphiFmx;
 
 type
   TPyMainForm = class(TForm)
@@ -72,17 +72,46 @@ type
     Label1: TLabel;
     PythonModule1: TPythonModule;
     PyDelphiWrapper1: TPyDelphiWrapper;
+    PythonDelphiVar1: TPythonDelphiVar;
+    PythonDelphiVar2: TPythonDelphiVar;
     procedure actRunExecute(Sender: TObject);
     procedure actClearOutputExecute(Sender: TObject);
     procedure actClearInputExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure PythonModule1Initialization(Sender: TObject);
+    procedure PythonDelphiVar1ExtSetData(Sender: TObject; Data: PPyObject);
+    procedure PythonDelphiVar1ExtGetData(Sender: TObject; var Data: PPyObject);
+    procedure PythonDelphiVar2ExtSetData(Sender: TObject; Data: PPyObject);
+    procedure PythonDelphiVar2ExtGetData(Sender: TObject; var Data: PPyObject);
   private
     FAppEnv: TAppEnvironment;
-    function AppEventHandler(AAppEvent: TApplicationEvent; AContext: TObject): boolean;
-    procedure ConfigurePython();
+    FSavedCameraActive : boolean;
+    FPermissionCamera: string;
+    FPyCamera: PPyObject;
+    FPyImage: PPyObject;
     procedure DisableComponents();
     procedure EnableComponents();
+    //App launch
+    procedure AppEventHandler(const Sender: TObject; const AMessage: TMessage);
+    //Python environment
+    procedure ConfigurePython();
+    //Device handlers
+    procedure ActivateCameraPermissionRequestResult(Sender: TObject;
+      const APermissions: TArray<string>;
+      const AGrantResults: TArray<TPermissionStatus>);
+    procedure DisplayRationale(Sender: TObject;
+      const APermissions: TArray<string>; const APostRationaleProc: TProc);
+    //Interop variables
+    function GetCameraComponent: TCameraComponent;
+    function GetImage: TImage;
+    //Interop methods
+    function AskDelphiForCameraPermission(PSelf, AArgs: PPyObject)
+      : PPyObject; cdecl;
+    function AskDelphiToShowFrame(PSelf, AArgs: PPyObject): PPyObject; cdecl;
+  protected
+    property Camera: TCameraComponent read GetCameraComponent;
+    property Image: TImage read GetImage;
   public
     { Public declarations }
   end;
@@ -93,7 +122,14 @@ var
 implementation
 
 uses
-  PythonLoad, System.IOUtils;
+  PythonLoad,
+  System.IOUtils,
+  FMX.DialogService,
+  Androidapi.Helpers,
+  Androidapi.JNI.JavaTypes,
+  Androidapi.JNI.Os,
+  WrapFMXMedia,
+  WrapFmxShapes;
 
 {$R *.fmx}
 
@@ -103,20 +139,72 @@ procedure TPyMainForm.FormCreate(Sender: TObject);
 begin
   DisableComponents();
   FAppEnv := TAppEnvironment.Create(TProgressViewFrame.Create(Self, Self));
-
-  var LAppEventService := IFMXApplicationEventService(nil);
-  if TPlatformServices.Current.SupportsPlatformService(
-    IFMXApplicationEventService, IInterface(LAppEventService)) then
-      LAppEventService.SetApplicationEventHandler(AppEventHandler)
-  else begin
-    Log.d('Platform service "IFMXApplicationEventService" not supported.');
-    Halt(1);
-  end;
+  TMessageManager.DefaultManager.SubscribeToMessage(TApplicationEventMessage, AppEventHandler);
+  FPermissionCamera := JStringToString(TJManifest_permission.JavaClass.Camera);
 end;
 
 procedure TPyMainForm.FormDestroy(Sender: TObject);
 begin
   FAppEnv.Free();
+end;
+
+function TPyMainForm.GetCameraComponent: TCameraComponent;
+begin
+  if IsDelphiObject(FPyCamera) then begin
+    Result := TPyDelphiCameraComponent(PythonToDelphi(FPyCamera)).DelphiObject
+  end else
+    Result := nil;
+end;
+
+function TPyMainForm.GetImage: TImage;
+begin
+  if IsDelphiObject(FPyImage) then
+    Result := TPyDelphiImage(PythonToDelphi(FPyImage)).DelphiObject
+  else
+    Result := nil;
+end;
+
+procedure TPyMainForm.PythonDelphiVar1ExtGetData(Sender: TObject;
+  var Data: PPyObject);
+begin
+  Data := FPyCamera;
+  PythonEngine1.Py_XINCREF(FPyCamera);
+end;
+
+procedure TPyMainForm.PythonDelphiVar1ExtSetData(Sender: TObject;
+  Data: PPyObject);
+begin
+  PythonEngine1.Py_XDECREF(FPyCamera);
+  FPyCamera := Data;
+  PythonEngine1.Py_XINCREF(FPyCamera);
+end;
+
+procedure TPyMainForm.PythonDelphiVar2ExtGetData(Sender: TObject;
+  var Data: PPyObject);
+begin
+  Data := FPyImage;
+  PythonEngine1.Py_XINCREF(FPyImage);
+end;
+
+procedure TPyMainForm.PythonDelphiVar2ExtSetData(Sender: TObject;
+  Data: PPyObject);
+begin
+  PythonEngine1.Py_XDECREF(FPyImage);
+  FPyImage := Data;
+  PythonEngine1.Py_XINCREF(FPyImage);
+end;
+
+procedure TPyMainForm.PythonModule1Initialization(Sender: TObject);
+begin
+  PythonModule1.AddDelphiMethod(
+    'AskDelphiForCameraPermission',
+    AskDelphiForCameraPermission,
+    'Check if camera permission has been granted.');
+
+  PythonModule1.AddDelphiMethod(
+    'AskDelphiToShowFrame',
+    AskDelphiToShowFrame,
+    'Displays sample buffer.');
 end;
 
 procedure TPyMainForm.actClearInputExecute(Sender: TObject);
@@ -129,29 +217,83 @@ begin
   mmOutput.Lines.Clear();
 end;
 
+procedure TPyMainForm.ActivateCameraPermissionRequestResult(Sender: TObject;
+  const APermissions: TArray<string>;
+  const AGrantResults: TArray<TPermissionStatus>);
+begin
+  if (Length(AGrantResults) = 1) and
+    (AGrantResults[0] = TPermissionStatus.Granted) then
+  begin
+    if Assigned(Camera) then
+      Camera.Active := True;
+    FSavedCameraActive := True;
+  end
+  else
+    TDialogService.ShowMessage
+      ('Cannot start the camera because the required permission has not been granted');
+end;
+
 procedure TPyMainForm.actRunExecute(Sender: TObject);
 begin
-  var LMainForm := PyDelphiWrapper1.Wrap(Self);
+  var
+  LMainForm := PyDelphiWrapper1.Wrap(Self);
   PythonModule1.SetVar('MainForm', LMainForm);
   PythonEngine1.Py_DECREF(LMainForm);
-
   PythonEngine1.CheckError();
   PythonEngine1.ExecString(AnsiString(mmInput.Lines.Text));
 end;
 
-function TPyMainForm.AppEventHandler(AAppEvent: TApplicationEvent;
-  AContext: TObject): boolean;
+procedure TPyMainForm.AppEventHandler(const Sender: TObject; const AMessage: TMessage);
 begin
-  case AAppEvent of
-    TApplicationEvent.FinishedLaunching: ConfigurePython();
+  case TApplicationEventMessage(AMessage).Value.Event of
+    TApplicationEvent.WillBecomeInactive:
+    begin
+      if Assigned(Camera) then
+      begin
+        FSavedCameraActive := Camera.Active;
+        Camera.Active := False;
+      end;
+    end;
+    TApplicationEvent.BecameActive:
+    begin
+      if Assigned(Camera) then
+      begin
+        Camera.Active := FSavedCameraActive;
+      end;
+    end;
+    TApplicationEvent.FinishedLaunching:
+      ConfigurePython();
   end;
-  Result := true;
+end;
+
+function TPyMainForm.AskDelphiForCameraPermission(PSelf, AArgs: PPyObject)
+  : PPyObject; cdecl;
+begin
+  if not PermissionsService.IsPermissionGranted(FPermissionCamera) then
+  begin
+    Result := PythonEngine1.ReturnFalse;
+    PermissionsService.RequestPermissions([FPermissionCamera],
+      ActivateCameraPermissionRequestResult, DisplayRationale);
+  end
+  else
+    Result := PythonEngine1.ReturnTrue;
+end;
+
+function TPyMainForm.AskDelphiToShowFrame(PSelf, AArgs: PPyObject): PPyObject;
+begin
+  TThread.Synchronize(TThread.Current,
+    procedure begin
+      if Camera.Active then
+        Camera.SampleBufferToBitmap(Image.Bitmap, true);
+    end);
+  Result := PythonEngine1.ReturnNone;
 end;
 
 procedure TPyMainForm.ConfigurePython;
 begin
-  FAppEnv.InitializeEnvironmentAsync(PythonEngine1, true,
-    procedure(const AInitialized: boolean; const ALastErrorMsg: string) begin
+  FAppEnv.InitializeEnvironmentAsync(PythonEngine1, True,
+    procedure(const AInitialized: boolean; const ALastErrorMsg: string)
+    begin
       if AInitialized then
         EnableComponents()
       else
@@ -170,11 +312,22 @@ begin
   btnClearInput.Enabled := false;
 end;
 
+procedure TPyMainForm.DisplayRationale(Sender: TObject;
+const APermissions: TArray<string>; const APostRationaleProc: TProc);
+begin
+  TDialogService.ShowMessage
+    ('The app needs to access the camera in order to work',
+    procedure(const AResult: TModalResult)
+    begin
+      APostRationaleProc;
+    end)
+end;
+
 procedure TPyMainForm.EnableComponents;
 begin
-  btnRun.Enabled := true;
-  btnClearOutput.Enabled := true;
-  btnClearInput.Enabled := true;
+  btnRun.Enabled := True;
+  btnClearOutput.Enabled := True;
+  btnClearInput.Enabled := True;
 end;
 
 end.
