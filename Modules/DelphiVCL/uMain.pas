17cd2a517bb0c4a10ceac3ec8eba8b970257fd50
lmbelo <lucas.belo@live.com>
2022-01-05 08:30:43 -0800

    Extending the moduledefs files with new env. config. options

--- Modules/DelphiVCL/uMain.pas
+++ Modules/DelphiVCL/uMain.pas
@@ -2,13 +2,13 @@ unit uMain;
 
 interface
 
-uses PythonEngine, System.SysUtils;
+uses PythonEngine;
 
 function PyInit_DelphiVCL: PPyObject; cdecl;
 
 implementation
 
-uses WrapDelphi, WrapDelphiVCL, System.IOUtils, System.JSON;
+uses System.IOUtils, System.SysUtils, System.JSON, WrapDelphi, WrapDelphiVCL;
 
 var
   gEngine : TPythonEngine;
@@ -20,18 +20,23 @@ begin
   Result := ExtractFilePath(GetModuleName(HInstance));
 end;
 
+function GetModuleDefsDir(): string;
+begin
+  Result := TDirectory.GetParent(ExcludeTrailingPathDelimiter(GetModuleDir()));
+end;
+
 function GetModuleDefsLogFilePath(): string;
 const
   MODULE_DEFS_LOG = 'moduledefs.log';
 begin
-  Result := TPath.Combine(GetModuleDir(), MODULE_DEFS_LOG);
+  Result := TPath.Combine(GetModuleDefsDir(), MODULE_DEFS_LOG, false);
 end;
 
 function GetModuleDefsJSONFilePath(): string;
 const
   MODULE_DEFS_JSON = 'moduledefs.json';
 begin
-  Result := TPath.Combine(GetModuleDir(), MODULE_DEFS_JSON);
+  Result := TPath.Combine(GetModuleDefsDir(), MODULE_DEFS_JSON, false);
 end;
 
 procedure Dump(const AText: string);
@@ -54,19 +59,42 @@ begin
 end;
 
 function TryLoadVerFromModuleDefs(): boolean;
-var
-  LPythonVer: string;
 begin
   //Load Python using the moduledefs.json file - it cracks the single version limitation
   var LFilePath := GetModuleDefsJSONFilePath();
   if TFile.Exists(LFilePath) then begin
     var LJson := TJSONObject.ParseJSONValue(TFile.ReadAllText(LFilePath));
+    if not Assigned(LJson) then
+      Exit(false);
+
+    var LPythonHome := String.Empty;
+    if LJson.TryGetValue<string>('python_home', LPythonHome) then begin
+      gEngine.PythonHome := LPythonHome;
+    end;
+
+    var LPythonBin := String.Empty;
+    if LJson.TryGetValue<string>('python_bin', LPythonBin) then begin
+      gEngine.ProgramName := LPythonBin;
+    end;
+
+    var LPythonLib := String.Empty;
+    if LJson.TryGetValue<string>('python_lib', LPythonLib) then begin
+      gEngine.DllPath := LPythonLib;
+    end;
+
+    var LPythonSharedLib := String.Empty;
+    if LJson.TryGetValue<string>('python_shared_lib', LPythonSharedLib) then begin
+      gEngine.DllName := LPythonSharedLib;
+    end;
+
+    var LPythonVer: string;
     if LJson.TryGetValue<string>('python_ver', LPythonVer) then begin
       for var I := Low(PYTHON_KNOWN_VERSIONS) to High(PYTHON_KNOWN_VERSIONS) do begin
         if (PYTHON_KNOWN_VERSIONS[I].RegVersion = LPythonVer) then begin
           gEngine.RegVersion := PYTHON_KNOWN_VERSIONS[I].RegVersion;
-          gEngine.DllName := PYTHON_KNOWN_VERSIONS[I].DllName;
-          Dump('Module has been set to Python' + gEngine.RegVersion);
+          if LPythonSharedLib.IsEmpty() then
+            gEngine.DllName := PYTHON_KNOWN_VERSIONS[I].DllName;
+          Dump(Format('Module has been set to Python %s using the module defs file', [gEngine.RegVersion]));
           Exit(true);
         end;
       end;
@@ -81,7 +109,7 @@ begin
   var PythonVersionIndex := {$I PythonVersionIndex.inc}; // 7 = 3.9
   gEngine.RegVersion := PYTHON_KNOWN_VERSIONS[PythonVersionIndex].RegVersion;
   gEngine.DllName := PYTHON_KNOWN_VERSIONS[PythonVersionIndex].DllName;
-  Dump('Module has been set to Python' + gEngine.RegVersion);
+  Dump(Format('Module has been set to Python %s using the version index file', [gEngine.RegVersion]));
   Exit(true);
 end;
 
@@ -102,20 +130,22 @@ begin
     gModule := TPythonModule.Create(nil);
     gModule.Engine := gEngine;
     // This must match the ProjectName and the function name pattern
-    gModule.ModuleName := 'DelphiVCL';
+    gModule.ModuleName := 'DelphiFMX';
 
     gDelphiWrapper := TPyDelphiWrapper.Create(nil);
     gDelphiWrapper.Engine := gEngine;
     gDelphiWrapper.Module := gModule;
 
     gEngine.LoadDll;
+    Result := gModule.Module;
   except
-    on E: Exception do
+    on E: Exception do begin
+      WriteLn('An error occurred: ' + E.Message);
       Dump(E.Message);
+      Result := gEngine.ReturnNone;
+    end;
   end;
-  Result := gModule.Module;
 end;
-
 
 initialization
   gEngine := nil;
