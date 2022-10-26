0b841dbf2c543033661ad87c73464d649e488c75
lmbelo <lucas.belo@live.com>
2022-10-26 11:36:24 -0300

    Including Python 3.7 for Android

--- Source/PythonEngine.pas
+++ Source/PythonEngine.pas
@@ -158,8 +158,9 @@ const
     );
 {$ENDIF}
 {$IFDEF ANDROID}
-  PYTHON_KNOWN_VERSIONS: array[6..9] of TPythonVersionProp =
+  PYTHON_KNOWN_VERSIONS: array[5..9] of TPythonVersionProp =
     (
+    (DllName: 'libpython3.7m.so'; RegVersion: '3.7'; APIVersion: 1013),
     (DllName: 'libpython3.8.so'; RegVersion: '3.8'; APIVersion: 1013),
     (DllName: 'libpython3.9.so'; RegVersion: '3.9'; APIVersion: 1013),
     (DllName: 'libpython3.10.so'; RegVersion: '3.10'; APIVersion: 1013),
