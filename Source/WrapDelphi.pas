f1a4945f9aa8670d57dc96294e9857833a75ceff
pyscripter <pyscripter@gmail.com>
2023-10-22 17:45:51 +0300

    Added the license header of Spring4D to the copied code.

--- Source/WrapDelphi.pas
+++ Source/WrapDelphi.pas
@@ -1053,6 +1053,29 @@ resourcestring
 
 
 {$REGION 'TRttiInvokableTypeHelper - "Lifted" from Spring4D"'}
+{***************************************************************************}
+{                                                                           }
+{           Spring Framework for Delphi                                     }
+{                                                                           }
+{           Copyright (c) 2009-2018 Spring4D Team                           }
+{                                                                           }
+{           http://www.spring4d.org                                         }
+{                                                                           }
+{***************************************************************************}
+{                                                                           }
+{  Licensed under the Apache License, Version 2.0 (the "License");          }
+{  you may not use this file except in compliance with the License.         }
+{  You may obtain a copy of the License at                                  }
+{                                                                           }
+{      http://www.apache.org/licenses/LICENSE-2.0                           }
+{                                                                           }
+{  Unless required by applicable law or agreed to in writing, software      }
+{  distributed under the License is distributed on an "AS IS" BASIS,        }
+{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
+{  See the License for the specific language governing permissions and      }
+{  limitations under the License.                                           }
+{                                                                           }
+{***************************************************************************}
 type
   TRttiInvokableTypeHelper = class helper for TRttiInvokableType
   public
