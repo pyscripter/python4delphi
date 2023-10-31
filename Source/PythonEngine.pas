f9be5062c62e91aec0ba3118259a38054a98b2d8
pyscripter <pyscripter@gmail.com>
2023-11-01 01:57:34 +0200

    A new function SafePyEngine was added.  It facilitates running python code in Delphi threads.

--- Source/PythonEngine.pas
+++ Source/PythonEngine.pas
@@ -2915,7 +2915,27 @@ function PythonVersionFromRegVersion(const ARegVersion: string;
   out AMajorVersion, AMinorVersion: integer): boolean;
 function PyStatus_Exception(const APyStatus: PyStatus): Boolean;
 
+//#######################################################
+//##                                                   ##
+//##        Support routines for running python        ##
+//##        code in threads                            ##
+//##                                                   ##
+//#######################################################
+
+type
+  IPyEngineAndGIL = interface
+    function GetPyEngine: TPythonEngine;
+    function GetThreadState: PPyThreadState;
+    property PythonEngine: TPythonEngine read GetPyEngine;
+    property ThreadState: PPyThreadState read GetThreadState;
+  end;
+
+// Access the PythonEngine with thread safety
+function SafePyEngine: IPyEngineAndGIL;
+
+
 { Helper functions}
+
 (*
     Checks whether the PythonVersion x.x is Registered
 *)
@@ -2923,6 +2943,7 @@ function PyStatus_Exception(const APyStatus: PyStatus): Boolean;
 function IsPythonVersionRegistered(PythonVersion : string;
   out InstallPath: string; out AllUserInstall: Boolean) : Boolean;
 {$ENDIF}
+
 (*
   Mask FPU Excptions - Useful for importing SciPy and other Python libs
   See http://bugs.python.org/issue9980 and
@@ -2930,6 +2951,7 @@ function IsPythonVersionRegistered(PythonVersion : string;
 *)
 procedure MaskFPUExceptions(ExceptionsMasked : boolean;
   MatchPythonPrecision : Boolean = True);
+
 (*
   Converts line breaks to LF and optionally adds a line break at the end
 *)
@@ -9787,6 +9809,54 @@ function PyStatus_Exception(const APyStatus: PyStatus): Boolean;
 begin
   Result := APyStatus._type <> _PyStatus_TYPE_OK;
 end;
+
+{ TPyEngineAndGIL - Internal class for SafePythonEngine }
+
+type
+  TPyEngineAndGIL = class(TInterfacedObject, IPyEngineAndGIL)
+  fPythonEngine: TPythonEngine;
+  fThreadState: PPyThreadState;
+  fGILState: PyGILstate_STATE;
+  private
+    function GetPyEngine: TPythonEngine;
+    function GetThreadState: PPyThreadState;
+  public
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+
+constructor TPyEngineAndGIL.Create;
+begin
+  inherited Create;
+  fPythonEngine := GetPythonEngine;
+  fGILState := fPythonEngine.PyGILState_Ensure;
+  fThreadState := fPythonEngine.PyThreadState_Get;
+end;
+
+destructor TPyEngineAndGIL.Destroy;
+begin
+  fPythonEngine.PyGILState_Release(fGILState);
+  inherited;
+end;
+
+function TPyEngineAndGIL.GetPyEngine: TPythonEngine;
+begin
+  Result := fPythonEngine;
+end;
+
+function TPyEngineAndGIL.GetThreadState: PPyThreadState;
+begin
+  Result := fThreadState;
+end;
+
+{ SafePythonEngine }
+function SafePyEngine: IPyEngineAndGIL;
+begin
+  Result := TPyEngineAndGIL.Create
+end;
+
+
 
 end.
 
